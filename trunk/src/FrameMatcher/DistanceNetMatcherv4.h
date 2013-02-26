e9950f364637ced7ce9fe7bdad7e2ec2e645d715
johan.ekekrantz@gmail.com <johan.ekekrantz@gmail.com@4d9638b0-5d75-c103-c8f2-f8e1c91d3fed>
2013-02-26 08:16:59 +0000

    ...

---
+++ trunk/src/FrameMatcher/DistanceNetMatcherv4.h
@@ -0,0 +1,74 @@
+#ifndef DistanceNetMatcherv4_H_
+#define DistanceNetMatcherv4_H_
+//OpenCV
+#include "cv.h"
+#include "highgui.h"
+#include <string>
+#include <iostream>
+#include <stdio.h>
+
+#include <pcl/io/pcd_io.h>
+#include <pcl/point_types.h>
+#include <pcl/registration/icp.h>
+#include <pcl/common/transformation_from_correspondences.h>
+#include <pcl/common/common_headers.h>
+#include <pcl/features/normal_3d.h>
+#include <pcl/io/pcd_io.h>
+#include <pcl/visualization/pcl_visualizer.h>
+#include <pcl/console/parse.h>
+
+#include <Eigen/Core>
+#include <Eigen/Geometry>
+#include <boost/thread/thread.hpp>
+
+#include "FrameMatcher.h"
+/*
+struct linkData {
+	float distance;
+	int id1;
+	int id2;
+};
+
+struct linkPair {
+	linkData * src_link;
+	linkData * dst_link;
+	float value;
+	float * p_src;
+	float * p_dst;
+	float * c_i;
+	float * c_j;
+};
+*/
+using namespace std;
+
+class DistanceNetMatcherv4: public FrameMatcher
+{
+	public:
+		int max_points;
+		int nr_iter;
+		float std_dist;
+		int lookup_size;
+		float bounds;
+		float probIgnore;
+
+		float * lookup;
+		float lookup_step;
+		
+		//float p1_rejection;
+		//float p2_rejection;
+		//float likelihood_rejection;
+		
+		bool movement_prior;
+		float movementPerFrame;
+				
+		bool feature_prior;
+		float feature_smoothing;
+		
+		DistanceNetMatcherv4(int iter_, int max_points_, float std_dist_, float bounds_, bool movement_prior_, float movementPerFrame_, bool feature_prior_, float feature_smoothing_);
+		
+		DistanceNetMatcherv4();
+		~DistanceNetMatcherv4();
+		Transformation * getTransformation(RGBDFrame * src, RGBDFrame * dst);
+};
+
+#endif
