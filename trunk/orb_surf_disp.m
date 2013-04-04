d83618b012c1bf75851244476778f868d810b1b3
johan.ekekrantz@gmail.com <johan.ekekrantz@gmail.com@4d9638b0-5d75-c103-c8f2-f8e1c91d3fed>
2013-04-04 08:56:36 +0000

    ...

--- trunk/orb_surf_disp.m
+++ trunk/orb_surf_disp.m
@@ -1,16 +1,25 @@
 run surf_data
 run orb_data
 
+AICK_surf_30_inf = originalAICKsurf_0_AICK_mat_pos;
+AICK_surf_5_200  = originalAICKsurf_1_AICK_mat_pos;
+
+AICK_orb_30_inf = originalAICKorb_0_AICK_mat_pos;
+AICK_orb_10_300  = originalAICKorb_1_AICK_mat_pos;
+
 disp('Running matlab script');
-%plot(log2(1:1024));
 %figure(1)
 %clf
 %hold on
-%step = 1:5:30;
-%plot(thresholds,originalAICKsurf_1_AICK_mat_pos(step,:),'g')
-%plot(thresholds,bowAICKsurf1000_bl_02_2_BowAICKv2_mat_pos(step,:),'g-x')
-%plot(thresholds,originalAICKorb_0_AICK_mat_pos(step,:),'r')
-%plot(thresholds,originalAICKorb_1_AICK_mat_pos(step,:),'r-x')
+part = 1:75;
+axis([0 thresholds(part(end)) 0 1.01])
+step = 1:5:30;
+plot(thresholds(part),AICK_surf_30_inf(step,(part)),'g')
+plot(thresholds(part),AICK_surf_5_200(step,(part)),'g-x')
+plot(thresholds(part),AICK_orb_30_inf(step,(part)),'r')
+plot(thresholds(part),AICK_orb_10_300(step,(part)),'r-x')
+legend('AICK surf','AICK surf fast','AICK orb','AICK orb fast')
+
 
 
 % figure(2)
